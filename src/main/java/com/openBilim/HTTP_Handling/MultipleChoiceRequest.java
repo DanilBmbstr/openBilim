1c25c9d59b8a811ab53741429bc5f02131fc404b
artem85wC <109022636+artem85wC@users.noreply.github.com>
2025-11-13 21:01:29 +0500

    Create MultipleChoiceRequest.java
    DTO для множественного выбора

---
+++ src/main/java/com/openBilim/HTTP_Handling/MultipleChoiceRequest.java
@@ -0,0 +1,10 @@
+// src/main/java/com/openBilim/HTTP_Handling/MultipleChoiceRequest.java
+package com.openBilim.HTTP_Handling;
+
+import java.util.List;
+
+public class MultipleChoiceRequest {
+    public String userToken;
+    public List<String> selectedOptions;   // например ["Меркурий", "Венера"]
+
+}
